6d4b39b53ce734ee56abd821d11d51ccc9212aca
Artem <adolya@users.noreply.github.com>
2023-02-13 10:11:33 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,10 @@
 output "terraform_version" {
   value = "v${data.external.terraform_version.result.version}"
 }
+
+
+output "common_tags" {
+  value = { 
+    terraform_version = "v${data.external.terraform_version.result.version}" 
+  }
+}
